0683ebe34db19a76012f751a53bd1d00bb7407f5
Moeketsi08 <moeketsi@royalmcgrady.co.za>
2024-11-15 14:09:02 +0200

    Update settings.py

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -93,8 +93,8 @@ WSGI_APPLICATION = 'schoolmanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('privatelink.postgres.database.azure.com'),
     }
 }
 
